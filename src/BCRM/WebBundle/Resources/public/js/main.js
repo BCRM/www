135f625bb961f43606e7bf4f0033dacdda378723
Markus Tacker <m@coderbyheart.de>
2013-09-22 23:09:42 +0200

    Slider nur auf der Startseite.

--- src/BCRM/WebBundle/Resources/public/js/main.js
+++ src/BCRM/WebBundle/Resources/public/js/main.js
@@ -10,31 +10,5 @@ Modernizr.load([
                 }
             });
         }
-    },
-    // Slider
-    {
-        test: Modernizr.mq('(min-width: 950px)'),
-        yep: [ '/bundles/bcrmweb/vendor/bxslider/jquery.bxslider.css', '/bundles/bcrmweb/css/slider.css', '/bundles/bcrmweb/vendor/bxslider/jquery.bxslider.js' ],
-        callback: function (url, result, key) {
-            if (!result || key < 2) return;
-            $.ajax(
-                '/c/Slider/Index',
-                {
-                    accepts: 'text/html',
-                    dataType: 'html',
-                    success: function (data) {
-                        var visual = $('div.visual');
-                        visual.html(data);
-                        visual.addClass('slider');
-                        $('.slider ul').bxSlider(
-                            {
-                                captions: true,
-                                pager: false
-                            }
-                        );
-                    }
-                }
-            );
-        }
     }
 ]);
