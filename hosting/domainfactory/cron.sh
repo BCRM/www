c21b7cef12621a33136508ffff64fb35e9665399
Markus Tacker <m@coderbyheart.de>
2013-10-13 16:04:39 +0200

    Updated.

--- hosting/domainfactory/cron.sh
+++ hosting/domainfactory/cron.sh
@@ -4,4 +4,9 @@
 
 cd `dirname $0`/../../
 app/console --env=prod bcrm:newsletter:confirm
+app/console --env=prod bcrm:registration:confirm
+app/console --env=prod bcrm:unregistration:confirm
+app/console --env=prod bcrm:tickets:process-unregistrations
+app/console --env=prod bcrm:tickets:create
+app/console --env=prod bcrm:tickets:notify
 app/console --env=prod swiftmailer:spool:send
