dbc28c309f3c318e36d940175d329113881c6b72
Markus Tacker <m@coderbyheart.de>
2013-10-15 15:03:17 +0200

    Newsletter command is no longer available.

--- hosting/domainfactory/cron.sh
+++ hosting/domainfactory/cron.sh
@@ -3,7 +3,6 @@
 . /kunden/303805_65187/.bashrc
 
 cd `dirname $0`/../../
-app/console --env=prod bcrm:newsletter:confirm
 app/console --env=prod bcrm:registration:confirm
 app/console --env=prod bcrm:unregistration:confirm
 app/console --env=prod bcrm:tickets:process-unregistrations
