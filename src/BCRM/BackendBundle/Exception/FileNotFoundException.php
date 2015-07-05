4fde31a3271adcb2cee6eab8bf537df86a172d72
Markus Tacker <m@coderbyheart.de>
2015-07-05 23:34:28 +0200

    Updated copyright.

--- src/BCRM/BackendBundle/Exception/FileNotFoundException.php
+++ src/BCRM/BackendBundle/Exception/FileNotFoundException.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * @author Markus Tacker <m@coderbyheart.de>
+ * @author    Markus Tacker <m@cto.hiv>
+ * @copyright 2013-2015 Verein zur Förderung der Netzkultur im Rhein-Main-Gebiet e.V. | http://netzkultur-rheinmain.de/
  */
 
 namespace BCRM\BackendBundle\Exception;
