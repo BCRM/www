0484dcb336907ea23024c4e5bc1c08e2f360462e
Markus Tacker <markus.tacker@fintura.de>
2015-10-05 20:50:03 +0200

    Added.

---
+++ hosting/cron-dev.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+app/console bcrm:payments:process --verbose
+app/console bcrm:registration:confirm --verbose
+app/console bcrm:registration:pay --verbose
+app/console bcrm:unregistration:confirm --verbose
+app/console bcrm:tickets:process-unregistrations --verbose
+app/console bcrm:tickets:create --verbose
+app/console bcrm:tickets:send --verbose
+app/console swiftmailer:spool:send
---
+++ hosting/cron-prod.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+app/console --env=prod bcrm:payments:process
+app/console --env=prod bcrm:registration:confirm
+app/console --env=prod bcrm:registration:pay
+app/console --env=prod bcrm:unregistration:confirm
+app/console --env=prod bcrm:tickets:process-unregistrations
+app/console --env=prod bcrm:tickets:create
+app/console --env=prod bcrm:tickets:send
+app/console --env=prod swiftmailer:spool:send > /dev/null
