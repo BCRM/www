8ea2e7361cae054398d5b8cb6374a29fe9a287ac
Markus Tacker <m@cto.hiv>
2015-11-13 19:05:11 +0100

    Change mailchimp list id.

--- hosting/domainfactory/cron-60m.sh
+++ hosting/domainfactory/cron-60m.sh
@@ -3,4 +3,4 @@
 . /kunden/303805_65187/.bashrc
 
 cd `dirname $0`/../../
-app/console --env=prod bcrm:mailchimp:update-participants-list 46004bc9ad
+app/console --env=prod bcrm:mailchimp:update-participants-list 633fd5a389
